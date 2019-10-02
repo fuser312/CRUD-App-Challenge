e723151a20c64d6357e1c71ec472480ac95e82a0
fuser <sahil.dogstar@gmail.com>
2019-10-02 08:47:31 +0530

    Finished this exercise.

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,29 @@
+import 'dart:io';
 
 void main(){
   List<String> names = ["Kimiko", "Raj", "Anu", "Paul"];
 
 // Write a program to allow user to modify the given list as shown below.
 // Add Student, remove student and stop the program
+  print("Do You want to (a)dd, (r)emove or (s)top?");
+  print(names);
+  String option = stdin.readLineSync();
+  if (option == "a"){
+    print("What is the name of the student you want to add?");
+    String addStudent = stdin.readLineSync();
+    names.add(addStudent);
+  }
 
+  else if (option == "r"){
+    print("What is the name of the student you want to remove?");
+    String addStudent = stdin.readLineSync();
+    names.remove(addStudent);
+  }
+
+  else if (option == "s"){
+    print("Stopping The Program");
+    exit(0);
+  }
 }
 
 //[Kimiko, Raj, Anu, Paul]
